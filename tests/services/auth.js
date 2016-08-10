31bcc4c68ce704ff4ac400623baf4191c0cc52c2
Little Buddha <littlebuddha31@gmail.com>
2016-08-10 18:21:50 +0200

    Add token link in email for user registration

---
+++ tests/services/auth.js
@@ -0,0 +1,27 @@
+import Promise from 'bluebird';
+import auth from '../../services/auth';
+import DB from '../../services/db-utils';
+import chai from 'chai';
+import config from '../../config';
+
+const should = chai.should();
+
+DB.init(config.db);
+
+function getId() {
+  return new Date().getTime().toString(36);
+}
+describe('Auth service', () => {
+
+  it('register', () => {
+    const userAttrs = {
+      email: 'benoithubert+' + getId() + '@gmail.com',
+      firstName: 'Toto',
+      lastName: 'Tau',
+      password: 'SecurePass123$$'
+    };
+    return auth.register(userAttrs)
+    .then(console.log);
+  });
+
+});
