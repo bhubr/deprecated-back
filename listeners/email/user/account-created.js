e150bea587b1650ee11b30dab49104810f31df28
Little Buddha <littlebuddha31@gmail.com>
2016-08-10 20:44:41 +0200

    Send html template

--- listeners/email/user/account-created.js
+++ listeners/email/user/account-created.js
@@ -1,5 +1,8 @@
+import mailgun from '../../../services/mailgun';
+
 module.exports = {
   handle(data) {
-    console.log('email:user:account-created', data);
+    // console.log('email:user:account-created', data);
+    mailgun.send(data);
   }
 }
--- services/mailgun.js
+++ services/mailgun.js
@@ -1,15 +1,21 @@
 import tokenService from './token';
+import fs from 'fs';
+import path    from 'path';
 const conf = require('../config.json').mailgun;
 const mailgun = require('mailgun-js')(conf);
 
+const templatePath = path.normalize(__dirname + '/../resources/templates/email.html');
+const template = fs.readFileSync(templatePath);
+
 export default {
 
-  sendEmail: function(params) {
+  send: function(params) {
     const data = {
       from: 'MyApp <benoithubert@samples.mailgun.org>',
       to: params.user.email, // 'benoithubert@gmail.com',
       subject: 'Hello',
-      text: 'Testing some Mailgun \nWelcome, ' + params.user.email + ', you just registered to my new super app.\n' + params.link
+      // text: 'Testing some Mailgun \nWelcome, ' + params.user.email + ', you just registered to my new super app.\n' + params.link
+      html: template.toString()
     };
 
     console.log(data);
